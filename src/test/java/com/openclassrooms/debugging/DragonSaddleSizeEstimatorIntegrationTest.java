194b2bb142554992236d6ca1bd6f6cb627f7c120
romainsessa <romainsessa@gmail.com>
2020-05-12 16:46:52 +0200

    Update test to year 2020.

--- src/test/java/com/openclassrooms/debugging/DragonSaddleSizeEstimatorIntegrationTest.java
+++ src/test/java/com/openclassrooms/debugging/DragonSaddleSizeEstimatorIntegrationTest.java
@@ -15,7 +15,7 @@ public class DragonSaddleSizeEstimatorIntegrationTest {
     @DisplayName("When the year is 2019 Then the saddle size should be 20.18 meters")
     @Test
     public void estimateSaddleSizeInCentiMeters_shouldReturnTwentyPointEighteenMeters_whenCalculatingTheSizeIn2019() throws Exception {
-        int targetYear = 2019;
+        int targetYear = 2020;
         // ARRANGE
         DragonSaddleSizeEstimator estimator = DragonSaddleSizeEstimator.INSTANCE;
 
@@ -23,6 +23,6 @@ public class DragonSaddleSizeEstimatorIntegrationTest {
         Double expectedSaddleSize = DragonSaddleSizeEstimator.INSTANCE.estimateSaddleSizeInCentiMeters(targetYear);
 
         // Assert
-        assertThat(expectedSaddleSize, is(equalTo(2018.0)));
+        assertThat(expectedSaddleSize, is(equalTo(2019.0)));
     }
 }
